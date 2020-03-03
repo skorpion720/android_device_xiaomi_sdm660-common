4cf63e6e9b7d8e516a777105ed905c371b557aa6
skorpion720 <40567079+skorpion720@users.noreply.github.com>
2020-03-03 18:22:13 +0200

    Initial commit

---
+++ extract-files.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE_COMMON=sdm660-common
+VENDOR=xiaomi
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+LINEAGE_ROOT="$MY_DIR"/../../..
+
+HELPER="$LINEAGE_ROOT"/vendor/carbon/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Default to sanitizing the vendor folder before extraction
+clean_vendor=true
+ONLY_COMMON=
+ONLY_DEVICE=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -o | --only-common )
+                ONLY_COMMON=false
+                ;;
+        -d | --only-device )
+                ONLY_DEVICE=false
+                ;;
+        -n | --no-cleanup )
+            CLEAN_VENDOR=false
+            ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                clean_vendor=false
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
+fi
+
+function blob_fixup() {
+    case "${1}" in
+
+    vendor/bin/mlipayd@1.1)
+        patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
+        ;;
+
+    vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so)
+        patchelf --remove-needed vendor.xiaomi.hardware.mtdservice@1.0.so "${2}"
+        sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
+        ;;
+
+    vendor/lib/hw/camera.sdm660.so)
+        patchelf --add-needed camera.sdm660_shim.so "${2}"
+        ;;
+
+    vendor/lib/hw/sound_trigger.primary.sdm660.so | vendor/lib64/hw/sound_trigger.primary.sdm660.so)
+        patchelf --add-needed libprocessgroup.so "${2}"
+        ;;
+
+    product/etc/permissions/vendor.qti.hardware.data.connection-V1.{0,1}-java.xml)
+        sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
+
+    esac
+}
+
+# Initialize the common helper
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true $clean_vendor
+
+if [ -z "${ONLY_DEVICE}" ] && [ -s "${MY_DIR}/proprietary-files.txt" ]; then
+extract "$MY_DIR"/proprietary-files.txt "$SRC" \
+    "${KANG}" --section "${SECTION}"
+fi
+
+if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+    # Reinitialize the helper for device
+    setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+    extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC" \
+    "${KANG}" --section "${SECTION}"
+fi
+
+"$MY_DIR"/setup-makefiles.sh
